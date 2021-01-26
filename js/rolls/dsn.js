2db3b6dbf9587552b46da2e85b54cff466c9fe49
Naethure <naethure@gmail.com>
2021-01-26 06:27:16 -0800

    Made it so public gm rolls should show 3d dice rolls now

--- js/rolls/dsn.js
+++ js/rolls/dsn.js
@@ -3,7 +3,8 @@ export async function rollDsN(dsnRolldata) {
 
   if (
     game.user.isGM &&
-    game.settings.get("dice-so-nice", "hideNpcRolls")
+    game.settings.get("dice-so-nice", "hideNpcRolls") &&
+    game.settings.get("core", "rollMode") != "public"
   )
     return;
   return game.dice3d.showForRoll(...dsnRolldata);
